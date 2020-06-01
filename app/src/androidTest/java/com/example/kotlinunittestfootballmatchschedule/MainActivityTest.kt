510725f1df501075d042abfd8bd1cf8ac69147b3
ferdiansyah <ferdinosaurus@gmail.com>
2020-06-01 21:16:22 +0700

    add test and library test

---
+++ app/src/androidTest/java/com/example/kotlinunittestfootballmatchschedule/MainActivityTest.kt
@@ -0,0 +1,42 @@
+package com.example.kotlinunittestfootballmatchschedule
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import com.example.kotlinunittestfootballmatchschedule.activity.MainActivity
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.util.regex.Pattern.matches
+
+import com.example.kotlinunittestfootballmatchschedule.R.id.*
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+    @Rule
+    @JvmField
+    var activityRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun testRecyclerViewBehaviour() {
+        onView(withId(rvLeague)).check(matches(isDisplayed()))
+        onView(withId(rvLeague)).perform(
+            RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
+                10
+            )
+        )
+        onView(withId(rvLeague)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                10,
+                click()
+            )
+        )
+    }
+
+}
